b8ecb9e629059721fed108b69bb95efaff022814
Edward Adasiak <eadasiak@adobe.com>
2020-04-21 15:32:23 -0700

    Fix for container name verfication on provision

--- pkg/services/storage/gpv2_blob_container_provision.go
+++ pkg/services/storage/gpv2_blob_container_provision.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/Azure/open-service-broker-azure/pkg/service"
 	uuid "github.com/satori/go.uuid"
 )
@@ -13,45 +12,13 @@ func (b *gpv2BlobContainerManager) GetProvisioner(
 	_ service.Plan,
 ) (service.Provisioner, error) {
 	return service.NewProvisioner(
-		service.NewProvisioningStep("checkNameAvailability", b.CheckNameAvailability),
 		service.NewProvisioningStep("preProvision", b.PreProvision),
 		service.NewProvisioningStep("deployARMTemplate", b.DeployARMTemplate),
 	)
 }
 
-func (b *gpv2BlobContainerManager) CheckNameAvailability(
-	_ context.Context,
-	instance service.Instance,
-) (service.InstanceDetails, error) {
-	containerName := instance.ProvisioningParameters.GetString("containerName")
-	if containerName == "" {
-		return nil, nil
-	}
-
-	pdt := instance.Parent.Details.(*instanceDetails)
-	client, _ := storage.NewBasicClient(
-		pdt.StorageAccountName,
-		pdt.AccessKey,
-	)
-	blobCli := client.GetBlobService()
-	response, err := blobCli.ListContainers(storage.ListContainersParameters{})
-	if err != nil {
-		return nil, fmt.Errorf("error checking name availability %s", err)
-	}
-	containers := response.Containers
-	for _, container := range containers {
-		if containerName == container.Name {
-			return nil, fmt.Errorf(
-				"container having name %s already exists in the storage account",
-				containerName,
-			)
-		}
-	}
-	return nil, nil
-}
-
 func (b *gpv2BlobContainerManager) PreProvision(
-	_ context.Context,
+	ctx context.Context,
 	instance service.Instance,
 ) (service.InstanceDetails, error) {
 	pdt := instance.Parent.Details.(*instanceDetails)
@@ -59,8 +26,30 @@ func (b *gpv2BlobContainerManager) PreProvision(
 		ARMDeploymentName:  uuid.NewV4().String(),
 		StorageAccountName: pdt.StorageAccountName,
 	}
-	if instance.ProvisioningParameters.GetString("containerName") != "" {
-		dt.ContainerName = instance.ProvisioningParameters.GetString("containerName")
+	requestedName := instance.ProvisioningParameters.GetString(
+		"containerName",
+	)
+	// If a specific name was requested, check availability
+	if requestedName != "" {
+		// First, retrieve the requested container name in the storage account
+		container, err := b.blobContainersClient.Get(
+			ctx,
+			instance.ProvisioningParameters.GetString("resourceGroup"),
+			pdt.StorageAccountName,
+			requestedName,
+		)
+		// If the name wasn't found (404), set the container name to requested
+		if container.Response.Response.StatusCode == 404 {
+			dt.ContainerName = requestedName
+		} else if container.ID != nil { // If the name was found, error out
+			return nil, fmt.Errorf(
+				"error with container name validation.  %s is already taken",
+				requestedName,
+			)	
+		} else if err != nil { // Otherwise, if we got an error, return it
+			return nil, fmt.Errorf("Error checking container name availability: %s", err)
+		} 
+	// A name wasn't requested, so assign one
 	} else {
 		dt.ContainerName = uuid.NewV4().String()
 	}
@@ -72,7 +61,6 @@ func (b *gpv2BlobContainerManager) DeployARMTemplate(
 	instance service.Instance,
 ) (service.InstanceDetails, error) {
 	dt := instance.Details.(*instanceDetails)
-
 	goTemplateParams := buildBlobContainerGoTemplate(
 		instance,
 		*instance.ProvisioningParameters,
@@ -82,7 +70,6 @@ func (b *gpv2BlobContainerManager) DeployARMTemplate(
 	for k := range tagsObj.Data {
 		tags[k] = tagsObj.GetString(k)
 	}
-
 	_, err := b.armDeployer.Deploy(
 		dt.ARMDeploymentName,
 		instance.ProvisioningParameters.GetString("resourceGroup"),
@@ -95,7 +82,6 @@ func (b *gpv2BlobContainerManager) DeployARMTemplate(
 	if err != nil {
 		return nil, fmt.Errorf("error deploying ARM template: %s", err)
 	}
-
 	return dt, nil
 }
 
@@ -104,7 +90,6 @@ func buildBlobContainerGoTemplate(
 	parameter service.ProvisioningParameters,
 ) map[string]interface{} {
 	dt := instance.Details.(*instanceDetails)
-
 	p := map[string]interface{}{}
 	p["storageAccountName"] = dt.StorageAccountName
 	p["containerName"] = parameter.GetString("containerName")
